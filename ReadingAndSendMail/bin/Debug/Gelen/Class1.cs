039c02f1dca2d3ac0d707ad7ee862db6de049791
Ali CAKIR <turkmuhendis@tukrmuhendis.net>
2021-08-20 22:29:22 +0300

    Gelen and Giden file added

---
+++ ReadingAndSendMail/bin/Debug/Gelen/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingAndSendMail.bin.Debug.Gelen
+{
+    class Class1
+    {
+    }
+}
